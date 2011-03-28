0f92e2e29f0f7fbc2353bf51ca6b4458824ae2b3
Ola Mork <ola@agoragames.com>
2011-03-28 06:31:38 -0400

    villagers are in fjords test

--- test/unit/village_test.rb
+++ test/unit/village_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class VillageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "village is in a fjord" do
+    user = Factory(:user, :nation => Factory(:nation))
+    user.fjords << Factory(:fjord, :nation => user.nation)
+    
+    assert_equal 0, user.fjords.first.villages.count
+    user.fjords.first.villages << Factory(:village)
+    assert_equal 1, user.fjords.first.villages.count
+    assert !user.fjords.first.villages.first.new_record?
+    assert user.fjords.first.villages.first.save
+    
+    assert_equal user.fjords.first.villages.first.fjord_id, user.fjords.first.id
   end
 end
