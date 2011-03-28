fd7a3f951751369be29ab668723f6aad8ec442ba
Ola Mork <ola@agoragames.com>
2011-03-27 21:00:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolarHunter::Application.config.secret_token = 'cc417308142eb62c52cc5769f838bf72bc43540871fec1bc279ff07d10b92c378584b623080ebf36ec913fea3533763fc795d61f92c60a971c2bef31de7da2bb'
