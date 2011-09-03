d721e495e423f650ca85c89acd84b267025c6c98
Ola Mork <ola@agoragames.com>
2011-09-03 16:50:23 -0400

    added a block for logged in users to home#index and fixed login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :token_authenticatable, :encryptable, :encryptor => :sha512
-
-  attr_protected :email, :password, :password_confirmation, :remember_me
   
   has_many :fjords
   has_many :ships, :through => :fjords
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,13 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_girl'
 
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
 class ActiveSupport::TestCase
   include FactoryGirl
-
+  
   def user_with_nation_and_fjord
     user = Factory(:user, :nation => Factory(:nation))
     user.fjords << Factory(:fjord, :nation => user.nation)
