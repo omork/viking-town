02aacf573249b6825d138e7b9424c2bda0dbbe42
Ola Mork <ola@agoragames.com>
2011-09-05 21:28:09 -0400

    moved resource test to village_resources_test

--- test/unit/village_resources_test.rb
+++ test/unit/village_resources_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class VillageResourcesTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "empty resources is created when village is created" do
+    user = user_with_nation_and_fjord
+    user.fjords.last.villages << Factory(:village)
+    
+    assert_equal Hash.new, user.fjords.last.villages.first.village_resources.resources
+    
+    # make sure the delegate is doing what we expect too
+    assert_equal Hash.new, user.fjords.last.villages.first.resources
   end
 end
--- test/unit/village_test.rb
+++ test/unit/village_test.rb
@@ -19,14 +19,4 @@ class VillageTest < ActiveSupport::TestCase
     
     assert_equal 1, user.fjords.last.villages.last.buildings.count
   end
-  
-  test "empty resources is created when village is created" do
-    user = user_with_nation_and_fjord
-    user.fjords.last.villages << Factory(:village)
-    
-    assert_equal Hash.new, user.fjords.last.villages.first.village_resources.resources
-    
-    # make sure the delegate is doing what we expect too
-    assert_equal Hash.new, user.fjords.last.villages.first.resources
-  end
 end
