3307b6bf5f9fcabf1b09420130bb20831d2574a6
Ola Mork <ola@agoragames.com>
2011-09-06 08:53:21 -0400

    -

--- app/models/buildings/round_house.rb
+++ app/models/buildings/round_house.rb
@@ -3,7 +3,7 @@ require 'building.rb'
 class RoundHouse < Building
   after_create :add_local_chief
   
-  Building::TASKS.merge!("RoundHouse" => %w(brew brawl wench))
+  Building::TASKS.merge!("RoundHouse" => %w(brew brawl))
   def add_local_chief
     self.villagers.create!(:title => :chief, :village_id => self.village_id)
   end
