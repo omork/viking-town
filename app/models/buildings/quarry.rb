7665b77b60d075f033fbfc74e46cfca942f7c3da
Ola Mork <ola@agoragames.com>
2011-09-19 21:57:15 -0400

    calculate rare and semi precious quary resources for each 'time'

--- app/models/buildings/quarry.rb
+++ app/models/buildings/quarry.rb
@@ -21,9 +21,10 @@ class Quarry < Building
       self.default_do_task('quarry-stone', Kernel.rand(10) * times)
       self.village.increment_resources('shaft-depth', times)
       
-      # TODO make this take into account "times"
-      self.village.increment_resources(RARE[Kernel.rand(RARE.size)], 1) if 1 == Kernel.rand(100)
-      self.village.increment_resources(SEMI_PRECIOUS[Kernel.rand(SEMI_PRECIOUS.size)], Kernel.rand(10)) if 1 == Kernel.rand(10)
+      1.upto(times) do
+        self.village.increment_resources(RARE[Kernel.rand(RARE.size)], 1) if 1 == Kernel.rand(100)
+        self.village.increment_resources(SEMI_PRECIOUS[Kernel.rand(SEMI_PRECIOUS.size)], Kernel.rand(10)) if 1 == Kernel.rand(10)
+      end
       
       self.village.increment_resources!(COMMON[Kernel.rand(COMMON.size)], Kernel.rand(5) * times)
     end
