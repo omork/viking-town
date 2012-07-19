8502d44007f9ada76b30ed0723ea20fa168798f6
Ola Mork <omork@mac.com>
2012-07-19 07:21:17 -0700

    changed the rare-item-calculation to have a much lower iteration limit (so if you come back in a year it won't run a billion times)

--- lib/rare_item_calculation.rb
+++ lib/rare_item_calculation.rb
@@ -1,18 +1,15 @@
 module RareItemCalculation
   # Figuring out rare items needs to happen for a lot of classes. Make it a module so I don't have to keep rewriting it.
-  def chances ; return { :rare => 100,  :semi_precious => 50,    :common => 3 } ; end
+  def chances ; return    { :rare => 100,  :semi_precious => 50,    :common => 3 } ; end
   def quantities ; return { :rare => 0..1, :semi_precious => 0..10, :common => 5..5 } ; end
-  def rarities ; return {} ; end
+  def rarities ; return   {} ; end
 
   def assign_rarities(village, times, rare = [], semi_precious = [], common = [])
-    results = {} # cache the results so we only do (at worst) rarities.keys.count updates to the village
+    results = {}
 
-    # don't allow an infinite calculation. If they've been gone for a month they don't get a giant chunk of resources
-    times = [times, 24 * 60 * 60 * 2 / 5].max
-
-    1.upto(times) do
-      chances.keys.each do |rarity|
-        next unless rarities.has_key?(rarity)
+    chances.keys.each do |rarity|
+      next unless rarities.has_key?(rarity)
+      0.upto(opportunitities_in(times)) do
         # the sample here ensures that we only add one rarity per iteration (so if you have only 1 "times" you can only get one of each rarity level)
         results[rarities[rarity].sample] = (results[rarity] || 0 ) + Building::random(quantities[rarity].max) + quantities[rarity].min
       end
@@ -27,5 +24,16 @@ module RareItemCalculation
   def assign_rarities!(village, times, rare = [], semi_precious = [], common = [])
     assign_rarities(village, times, rare, semi_precious, common)
     village.village_resources.save!
+  end
+
+private
+  # this makes our worst case iteration 29 * chances.keys * number of buildings in village
+  def opportunitities_in(times)
+    return case times
+    when 0..600 then 1
+    when 601..1800 then 2
+    when 1801..3600 then 3
+    when 3601..86400 then times / 3600 + 4
+    else 29 end
   end
 end
