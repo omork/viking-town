ee6c1f1aaf7a9f36a74567839b6bdab9c1b7ce2a
Ola Mork <omork@mac.com>
2012-07-28 21:17:44 -0700

    go directly to your first village upon login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index ; end
+  def index
+    if user_signed_in?
+      begin
+        redirect_to :controller => :village, :action => :show, :id => current_user.villages.first.id
+        return
+      rescue RuntimeError
+      end
+    end
+  end
 end
