c04797c1fb02d981b0d1a8fa32c87926d0223a03
Ola Mork <omork@mac.com>
2013-05-18 08:27:56 -0700

    cleaned up blacksmith tests some more (to be generic instead of one test per manufacturable)

--- test/unit/blacksmith_test.rb
+++ test/unit/blacksmith_test.rb
@@ -1,21 +1,55 @@
 require 'test_helper'
 
 class BlacksmithTest < ActiveSupport::TestCase
-  test "Swords consume 10 wrought-irons & if there are only 10 available it'll only make 1 sword" do
-    user = user_with_nation_and_fjord
-    village = village_with_single_villager_for_user(user)
-    
-    village.buildings << Blacksmith.create!(:x => 1, :y => 1)
-
-    # preload a bunch of resources
-    %w(coal planks wrought-iron copper).each do |resource|
+  def preload_resources(village)
+    %w(coal planks wrought-iron copper iron iron-ingots).each do |resource|
       village.increment_resources!(resource, 10)
     end
+    village
+  end
 
-    blacksmith = offset_assigned_at(village.buildings.last, 501) { |bs| bs.allocate('sword') }
+  def setup_user_and_village_with_blacksmith
+    user = user_with_nation_and_fjord
+    village = village_with_single_villager_for_user(user)
+    village.buildings << Blacksmith.create!(:x => 1, :y => 1)
+    village = preload_resources(village)
+    return user, village
+  end
+
+  test "manufacturables consume the correct amount of resources" do
+    user, village = setup_user_and_village_with_blacksmith
+    blacksmith = village.buildings.last
+
+    blacksmith.manufacturables.each do |item, resources|
+      village.village_resources.resources = {}
+      village = preload_resources(village)
+
+      blacksmith = offset_assigned_at(blacksmith, 501) { |bs| bs.allocate(item) }
+      village.reload
+      needs = %w(coal planks wrought-iron copper).zip(resources)
+      needs.each do |need, value|
+        assert_equal 10 - value, village.resources[need], "#{need} : #{item}"
+      end
+
+      assert_equal 1, village.resources[item], item
+    end
+  end
+
+  test "smelting consumes coal and produces ingots" do
+    user, village = setup_user_and_village_with_blacksmith
+    blacksmith = offset_assigned_at(village.buildings.last, 501) { |bs| bs.allocate('smelt') }
     village.reload
-    assert_equal 0, village.resources['wrought-iron']
-    assert_equal 1, village.resources['sword']
+    assert_equal 9, village.resources['coal']
+    assert_equal 8, village.resources['iron']
+    assert_equal 12, village.resources['iron-ingots']
+  end
+
+  test "wrought'ing consumes coal and ingots and produces wrought-iron" do
+    user, village = setup_user_and_village_with_blacksmith
+    blacksmith = offset_assigned_at(village.buildings.last, 501) { |bs| bs.allocate('wrought') }
+    village.reload
+    assert_equal 9, village.resources['coal']
+    assert_equal 9, village.resources['iron-ingots']
+    assert_equal 11, village.resources['wrought-iron']
   end
 end
-
