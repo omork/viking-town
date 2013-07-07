a1b0dcbab002ec1c9ae753d131cf9dfdfa398a2a
Ola Mork <omork@mac.com>
2013-07-06 22:37:05 -0700

    cleaned up sacrifice tests

--- test/models/sacrifice_test.rb
+++ test/models/sacrifice_test.rb
@@ -1,21 +1,22 @@
 require 'test_helper'
 
 class SacrificeTest < ActiveSupport::TestCase
-  test "take_resources_from_village gets all of them" do
+  def setup_sacrifice
     user, village = user_and_village
     village.resources['wood'] = 200
     assert village.village_resources.save
     sacrifice = Sacrifice.on_behalf_of(village).these_goods(wood: 20)
+  end
+
+  test "take_resources_from_village gets all of them" do
+    setup_sacrifice
     assert sacrifice.save!
     village.reload
     assert village.resources['wood'] == 180
   end
 
   test "take_resources_from_village doesn't let the sacrifice save if there aren't enough resources" do
-    user, village = user_and_village
-    village.resources['wood'] = 200
-    assert village.village_resources.save
-    sacrifice = Sacrifice.on_behalf_of(village).these_goods(wood: 2000)
+    setup_sacrifice
     assert sacrifice.save == false
     village.reload
     assert village.resources['wood'] == 200
